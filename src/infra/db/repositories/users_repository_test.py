125e416b185c8805d1df3fae4fa3df812e2c117d
RudhyDev <rudhy.pereira@gmail.com>
2023-07-08 21:40:09 -0300

    feature: ✨ finishing user_repository_test

--- src/infra/db/repositories/users_repository_test.py
+++ src/infra/db/repositories/users_repository_test.py
@@ -1,10 +1,40 @@
+import pytest
+from sqlalchemy import text
+from src.infra.db.settings.connection import DBConnectionHandler
 from .users_repository import UsersRepository
 
+db_connection_handler = DBConnectionHandler()
+connection = db_connection_handler.get_engine().connect()
 
+@pytest.mark.skip(reason='Sensive Test')
 def test_insert_user():
-    mocked_first_name = 'valid_name'
-    mocked_last_name = 'valid_last_name'
+    mocked_first_name = 'first'
+    mocked_last_name = 'last'
     mocked_age = 18
 
     users_repository = UsersRepository()
     users_repository.insert_user(mocked_first_name, mocked_last_name, mocked_age)
+
+    sql = ''' 
+        SELECT * FROM users 
+        WHERE first_name = "{}" 
+        AND last_name = "{}" 
+        AND age = "{}"
+        '''.format(mocked_first_name, mocked_last_name, mocked_age )
+        
+    response = connection.execute(text(sql))
+    registry = response.fetchall()[0]
+    
+    print('Resultado do registro aqui: ', registry)
+    
+    assert registry.first_name == mocked_first_name
+    assert registry.last_name == mocked_last_name
+    assert registry.age == mocked_age
+    
+    print('assert registry.first_name == mocked_first_name: ', 
+          registry.first_name == mocked_first_name)
+    
+    deletado = connection.execute(text('''DELETE FROM users'''))
+
+    print('Quantidade de linhas deletadas: ', deletado.rowcount)
+    connection.commit()
